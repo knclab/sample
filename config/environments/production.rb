acf4d2631840d7dd4f5e69ccaec27e4e655db0b9
kanouchi <kanouchi@matchalarm.co.jp>
2015-08-20 15:45:54 +0900

    up

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # /public配下の性的ページ呼び出し
+  config.serve_static_assets = true
 end
